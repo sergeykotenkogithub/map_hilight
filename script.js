ef97d82bbd32965f73ee0497c3e27d2e87f2cd1f
sergeykotenko <sergeykotenko@bk.ru>
2021-04-01 03:09:50 +0300

    change maphilight

--- script.js
+++ script.js
@@ -7,25 +7,71 @@ let abb4 = document.querySelector(".tt4");
 let map_roof = document.querySelector(".map_roof_div"); // c рефлёной крышей
 let abb6 = document.querySelector(".tt6");
 
+
 // Общая функция для изменения выделения и вывода блока при нажатии, d - это для кнопок отдельно
+
+// Удаление при нажатие
 
 function map_hilight(a, b, c, d) {
   let map_choose = document.querySelector(d);
   map_choose.addEventListener("click", (e) => {
+    e.preventDefault();
     b.classList.toggle(c);
     var data = $(a).mouseout().data("maphilight") || {};
     data.alwaysOn = !data.alwaysOn;
     $(a).data("maphilight", data).trigger("alwaysOn.maphilight");
-	
+    if (a === ".map_main") {
+      map_roof.classList.remove("map_roof_display")           
+      abb6.classList.remove("z6")           
+      var datas = $(".map_roof").mouseout().data("maphilight") || {};
+      var datas2 = $(".ns6").mouseout().data("maphilight") || {};
+      datas.alwaysOn = false;
+      datas2.alwaysOn = false;
+      $(".map_roof ").data("maphilight", datas).trigger("alwaysOn.maphilight");
+      $(".ns6 ").data("maphilight", datas2).trigger("alwaysOn.maphilight");
+    }
+    if (a === ".map_roof") {
+      map_main.classList.remove("map_main_display") 
+      abb6.classList.remove("z6")    
+      var datas = $(".map_main").mouseout().data("maphilight") || {};
+      var datas2 = $(".ns6").mouseout().data("maphilight") || {};
+      datas.alwaysOn = false;
+      datas2.alwaysOn = false
+      $(".map_main").data("maphilight", datas).trigger("alwaysOn.maphilight");
+      $(".ns6 ").data("maphilight", datas2).trigger("alwaysOn.maphilight");
+    }
+    if (a === ".ns6") {
+      map_main.classList.remove("map_main_display") 
+      map_roof.classList.remove("map_roof_display")      
+      var datas = $(".map_main").mouseout().data("maphilight") || {};
+      var datas2 = $(".map_roof").mouseout().data("maphilight") || {};
+      datas.alwaysOn = false;
+      datas2.alwaysOn = false
+      $(".map_main").data("maphilight", datas).trigger("alwaysOn.maphilight");
+      $(".map_roof ").data("maphilight", datas2).trigger("alwaysOn.maphilight");
+    }
   });
 }
+
+// Просто выделение
+
+// function map_hilight(a, b, c, d) {
+//   let map_choose = document.querySelector(d);
+//   map_choose.addEventListener("click", (e) => {
+//     e.preventDefault();
+//     b.classList.toggle(c);
+//     var data = $(a).mouseout().data("maphilight") || {};
+//     data.alwaysOn = !data.alwaysOn;
+//     $(a).data("maphilight", data).trigger("alwaysOn.maphilight");  
+//   });
+// }
 
 //  Функции
 
 // map_hilight(".map_little", abb, "z1", ".map_little"); // Мелкое здание
 map_hilight(".map_main", map_main, "map_main_display", ".map_main"); // Большое здание !!!!
-map_hilight(".ns3", abb3, "z3", ".ns3"); // Чуть выше большого
-map_hilight(".ns4", abb4, "z4", ".ns4"); // Большое дальнее
+// map_hilight(".ns3", abb3, "z3", ".ns3"); // Чуть выше большого
+// map_hilight(".ns4", abb4, "z4", ".ns4"); // Большое дальнее
 map_hilight(".map_roof", map_roof, "map_roof_display", ".map_roof"); // С ребристой крышой здание !!!!
 map_hilight(".ns6", abb6, "z6", ".ns6"); // Самое самое
 
